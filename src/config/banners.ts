c56bbe0d911dd90f0b9c3613ea9505616da79922
Sennett Lau <laub1199@gmail.com>
2024-04-23 00:02:33 +0800

    banner order update

--- src/config/banners.ts
+++ src/config/banners.ts
@@ -1,22 +1,22 @@
 export const bannerConfig = [
   {
-    name: 'hoponopono_001',
-    by: 'hoponopono88',
+    name: 'banner_001',
+    by: 'inkonbtc',
   },
   {
     name: 'hoponopono_002',
     by: 'hoponopono88',
   },
   {
-    name: 'banner_001',
-    by: 'inkonbtc',
+    name: 'rift_001',
+    by: 'entertherifts',
   },
   {
     name: 'banner_002',
     by: 'inkonbtc',
   },
   {
-    name: 'rift_001',
-    by: 'entertherifts',
+    name: 'hoponopono_001',
+    by: 'hoponopono88',
   },
 ]
