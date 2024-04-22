14df33c3e81b9c47d7c13f13cdbc135caea7fb55
Sennett Lau <laub1199@gmail.com>
2024-04-23 00:51:06 +0800

    added ink slider

--- src/components/canvas/InkCanvas.tsx
+++ src/components/canvas/InkCanvas.tsx
@@ -12,6 +12,7 @@ const InkCanvas = (props: Props) => {
   const { banner, ink, canvasRef, containerRef } = props
 
   const [containerWidth, setContainerWidth] = useState<number>(0)
+  const [inkXPosition, setInkXPosition] = useState<number>(0)
 
   useEffect(() => {
     const handleResize = () => {
@@ -30,12 +31,14 @@ const InkCanvas = (props: Props) => {
     if (containerWidth > 0) {
       drawImages()
     }
-  }, [banner, ink, containerWidth])
+  }, [banner, ink, containerWidth, inkXPosition])
 
   const drawImages = () => {
     const canvas = canvasRef.current
     if (canvas && banner && ink && containerWidth) {
       const ctx = canvas.getContext('2d')
+
+      const inkMaxX = canvas.width - canvas.width / 3
 
       canvas.width = containerWidth
       canvas.height = containerWidth / 3
@@ -55,7 +58,7 @@ const InkCanvas = (props: Props) => {
           inkImage.src = `/assets/inks/${ink}.webp`
 
           inkImage.onload = () => {
-            ctx.drawImage(inkImage, canvas.width - canvas.height - canvas.width / 10, 0, canvas.height, canvas.height)
+            ctx.drawImage(inkImage, Math.min(inkXPosition, inkMaxX), 0, canvas.height, canvas.height)
           }
         }
       }
@@ -63,8 +66,22 @@ const InkCanvas = (props: Props) => {
   }
 
   return (
-    <div ref={containerRef} className='w-full max-w-[800px]'>
-      <canvas ref={canvasRef} className='border'></canvas>
+    <div className={`w-full max-w-[800px]`}>
+      <div
+        ref={containerRef}
+        className={`w-full`}
+        style={{ backgroundImage: `url(/assets/banner-backgrounds/${banner}.webp)`, backgroundSize: 'contain' }}
+      >
+        <canvas ref={canvasRef} className='border'></canvas>
+      </div>
+      <input
+        type='range'
+        min='0'
+        max={containerWidth - containerWidth / 3} // Update max based on current canvas dimensions
+        value={inkXPosition}
+        onChange={(e) => setInkXPosition(parseInt(e.target.value))}
+        className='w-full accent-black'
+      />
     </div>
   )
 }
