ff0bd8535e7258601c03bdbbd5361d8788b4ea8c
Sennett Lau <laub1199@gmail.com>
2024-04-22 15:57:13 +0800

    added InkCanvas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
-import Image from 'next/image'
+'use client'
+
+import { useState } from 'react'
+import InkCanvas from '../components/canvas/InkCanvas'
 
 export default function Home() {
+  const [bannerBackground, setBannerBackground] = useState<string>('banner_001')
+  const [inkId, setInkId] = useState<string>('1')
+
   return (
     <div className='w-full flex-1 flex justify-center items-center'>
-      Body
+      <div className='w-full flex-1 max-w-[1440px] mx-auto px-6 py-8'>
+        <div className='flex w-2/3 justify-center items-center'>
+          <InkCanvas banner={bannerBackground} ink={inkId} />
+        </div>
+        <div className='w-1/3'></div>
+      </div>
     </div>
   )
 }
---
+++ src/components/canvas/InkCanvas.tsx
@@ -0,0 +1,82 @@
+'use client'
+import { useEffect, useRef, useState } from 'react'
+
+type Props = {
+  banner: string
+  ink: string
+}
+
+const InkCanvas = (props: Props) => {
+  const { banner, ink } = props
+
+  const canvasRef = useRef<HTMLCanvasElement>(null)
+  const containerRef = useRef<HTMLDivElement>(null)
+  const [containerWidth, setContainerWidth] = useState<number>(0)
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (containerRef.current) {
+        setContainerWidth(containerRef.current.offsetWidth)
+      }
+    }
+
+    window.addEventListener('resize', handleResize)
+    handleResize()
+
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+
+  useEffect(() => {
+    if (containerWidth > 0) {
+      drawImages()
+    }
+  }, [banner, ink, containerWidth])
+
+  const drawImages = () => {
+    const canvas = canvasRef.current
+    if (canvas && banner && ink && containerWidth) {
+      const ctx = canvas.getContext('2d')
+
+      canvas.width = containerWidth
+      canvas.height = containerWidth / 3
+
+      if (ctx) {
+        const baseImage = new Image()
+        const inkImage = new Image()
+
+        baseImage.src = `/assets/banner-backgrounds/${banner}.webp`
+        inkImage.src = `/assets/inks/${ink}.webp`
+
+        console.log(`Using banner: ${baseImage.src} and ink: ${inkImage.src}`)
+
+        baseImage.onload = () => {
+          ctx.clearRect(0, 0, canvas.width, canvas.height)
+          ctx.drawImage(baseImage, 0, 0, canvas.width, canvas.height)
+        }
+
+        inkImage.onload = () => {
+          ctx.drawImage(inkImage, canvas.width - canvas.height - canvas.width / 10, 0, canvas.height, canvas.height)
+        }
+      }
+    }
+  }
+
+  const downloadImage = () => {
+    const canvas = canvasRef.current
+    if (canvas) {
+      const image = canvas.toDataURL('image/png')
+      const link = document.createElement('a')
+      link.href = image
+      link.download = 'combined-image.png'
+      link.click()
+    }
+  }
+
+  return (
+    <div ref={containerRef} className='w-full max-w-[800px]'>
+      <canvas ref={canvasRef} className='border'></canvas>
+    </div>
+  )
+}
+
+export default InkCanvas
