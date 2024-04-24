7fe5b84ab8370efa29c73a35c18f72a95819ccbf
Sennett Lau <laub1199@gmail.com>
2024-04-24 16:18:52 +0800

    more on code refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,12 @@
 'use client'
 
-import Image from 'next/image'
-import { useRef, useState } from 'react'
+import { useState } from 'react'
 import CanvasContainer from '../components/canvas/CanvasControl'
-import InkCanvas from '../components/canvas/InkCanvas'
-import { bannerConfig } from '../config/banners'
+import XBannerCustomizer from '../components/customizer/XBannerCustomizer'
 
 export default function Home() {
-  const canvasRef = useRef<HTMLCanvasElement>(null)
-  const containerRef = useRef<HTMLDivElement>(null)
-
   const [bannerBackground, setBannerBackground] = useState<string>('banner_001')
   const [inkId, setInkId] = useState<string>('1')
-
-  const [bannerPage, setBannerPage] = useState<number>(1)
-  const [bannerTotalPages, setBannerTotalPages] = useState<number>(Math.ceil(bannerConfig.length / 3))
-
-  const banners = bannerConfig
-
-  const downloadImage = () => {
-    const canvas = canvasRef.current
-    if (canvas) {
-      console.log('Downloading image')
-      const image = canvas.toDataURL('image/png')
-      const link = document.createElement('a')
-      link.href = image
-      link.download = `inkonbtc-${bannerBackground}-ink_${inkId}.png`
-      link.click()
-    }
-  }
-
-  const onBannerSelect = (banner: string) => {
-    setBannerBackground(banner)
-
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth',
-    })
-  }
 
   return (
     <div className='w-full flex-1 flex justify-center items-center py-10 lg:py-0'>
@@ -46,68 +15,12 @@ export default function Home() {
           <CanvasContainer inkId={inkId} bannerBackground={bannerBackground} />
         </div>
         <div className='w-fit max-w-full lg:w-1/3 lg:px-2 xl:px-8 mx-auto'>
-          <div className='flex flex-col w-full rounded-2xl bg-white bg-opacity-50 px-4 py-4 shadow-xl min-h-[400px]'>
-            <div className='flex gap-2'>
-              <label className='text-black italic font-bold pointer' htmlFor='ink-id'>
-                INK #:
-              </label>
-              <input
-                className='bg-transparent border-b-2 border-black text-black font-bold italic pl-2 focus:outline-none'
-                type='text'
-                id='ink-id'
-                value={inkId}
-                onChange={(e) => setInkId(e.target.value)}
-              />
-            </div>
-            <p className='text-black italic font-bold text-lg mt-4 w-full text-left'>Choose a banner Background:</p>
-            <div className='flex flex-col gap-2 max-w-full'>
-              <div className='flex flex-col gap-2 w-full lg:min-h-[376px]'>
-                {banners.slice((bannerPage - 1) * 3, bannerPage * 3).map((banner, index) => (
-                  <div className='max-w-full w-[365px] h-[120px] relative transform hover:scale-105 transition-transform duration-300'>
-                    <a
-                      className='italic text-black text-md bg-white bg-opacity-75 rounded-br-lg p-2 absolute top-0 left-0'
-                      href={`https://x.com/${banner.by}`}
-                    >
-                      @{banner.by}
-                    </a>
-                    <Image
-                      className={`cursor-pointer ${bannerBackground === banner.name ? 'max-w-full border-2 border-primary-500' : ''}`}
-                      key={index}
-                      src={`/assets/banner-backgrounds/${banner.name}.webp`}
-                      alt={banner.name}
-                      width={365}
-                      height={120}
-                      onClick={() => onBannerSelect(banner.name)}
-                    />
-                  </div>
-                ))}
-              </div>
-              <div className='flex'>
-                <p className='text-black italic font-bold w-full text-left'>
-                  Page {bannerPage} of {bannerTotalPages}
-                </p>
-                <button
-                  onClick={() => setBannerPage((prev) => (prev === 1 ? prev : prev - 1))}
-                  className='mr-2 text-black font-bold'
-                >
-                  {'<'}
-                </button>
-                <input
-                  className='bg-transparent border-b-2 border-black text-black font-bold pl-2 focus:outline-none w-[24px]'
-                  type='text'
-                  value={bannerPage}
-                  onChange={(e) => setBannerPage(parseInt(e.target.value))}
-                  disabled
-                />
-                <button
-                  onClick={() => setBannerPage((prev) => (prev === bannerTotalPages ? prev : prev + 1))}
-                  className='ml-2 text-black font-bold'
-                >
-                  {'>'}
-                </button>
-              </div>
-            </div>
-          </div>
+          <XBannerCustomizer
+            inkId={inkId}
+            setInkId={setInkId}
+            bannerBackground={bannerBackground}
+            setBannerBackground={setBannerBackground}
+          />
         </div>
       </div>
     </div>
---
+++ src/components/customizer/XBannerCustomizer.tsx
@@ -0,0 +1,97 @@
+import Image from 'next/image'
+import { useState } from 'react'
+import { bannerConfig } from '../../config/banners'
+
+type Props = {
+  inkId: string
+  setInkId: (inkId: string) => void
+  bannerBackground: string
+  setBannerBackground: (bannerBackground: string) => void
+}
+
+const XBannerCustomizer = (props: Props) => {
+  const { inkId, bannerBackground, setInkId, setBannerBackground } = props
+
+  const [bannerPage, setBannerPage] = useState<number>(1)
+  const [bannerTotalPages, setBannerTotalPages] = useState<number>(Math.ceil(bannerConfig.length / 3))
+
+  const banners = bannerConfig
+
+  const onBannerSelect = (banner: string) => {
+    setBannerBackground(banner)
+
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    })
+  }
+
+  return (
+    <>
+      <div className='flex flex-col w-full rounded-2xl bg-white bg-opacity-50 px-4 py-4 shadow-xl min-h-[400px]'>
+        <div className='flex gap-2'>
+          <label className='text-black italic font-bold pointer' htmlFor='ink-id'>
+            INK #:
+          </label>
+          <input
+            className='bg-transparent border-b-2 border-black text-black font-bold italic pl-2 focus:outline-none'
+            type='text'
+            id='ink-id'
+            value={inkId}
+            onChange={(e) => setInkId(e.target.value)}
+          />
+        </div>
+        <p className='text-black italic font-bold text-lg mt-4 w-full text-left'>Choose a banner Background:</p>
+        <div className='flex flex-col gap-2 max-w-full'>
+          <div className='flex flex-col gap-2 w-full lg:min-h-[376px]'>
+            {banners.slice((bannerPage - 1) * 3, bannerPage * 3).map((banner, index) => (
+              <div className='max-w-full w-[365px] h-[120px] relative transform hover:scale-105 transition-transform duration-300'>
+                <a
+                  className='italic text-black text-md bg-white bg-opacity-75 rounded-br-lg p-2 absolute top-0 left-0'
+                  href={`https://x.com/${banner.by}`}
+                >
+                  @{banner.by}
+                </a>
+                <Image
+                  className={`cursor-pointer ${bannerBackground === banner.name ? 'max-w-full border-2 border-primary-500' : ''}`}
+                  key={index}
+                  src={`/assets/banner-backgrounds/${banner.name}.webp`}
+                  alt={banner.name}
+                  width={365}
+                  height={120}
+                  onClick={() => onBannerSelect(banner.name)}
+                />
+              </div>
+            ))}
+          </div>
+          <div className='flex'>
+            <p className='text-black italic font-bold w-full text-left'>
+              Page {bannerPage} of {bannerTotalPages}
+            </p>
+            <button
+              onClick={() => setBannerPage((prev) => (prev === 1 ? prev : prev - 1))}
+              className='mr-2 text-black font-bold'
+            >
+              {'<'}
+            </button>
+            <input
+              className='bg-transparent border-b-2 border-black text-black font-bold pl-2 focus:outline-none w-[24px]'
+              type='text'
+              value={bannerPage}
+              onChange={(e) => setBannerPage(parseInt(e.target.value))}
+              disabled
+            />
+            <button
+              onClick={() => setBannerPage((prev) => (prev === bannerTotalPages ? prev : prev + 1))}
+              className='ml-2 text-black font-bold'
+            >
+              {'>'}
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default XBannerCustomizer
