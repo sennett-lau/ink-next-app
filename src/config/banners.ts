f274a096e66482f549c0a35a9bc2606053727018
Sennett Lau <laub1199@gmail.com>
2024-04-24 17:29:17 +0800

    added banners

--- src/config/banners.ts
+++ src/config/banners.ts
@@ -4,8 +4,48 @@ export const bannerConfig = [
     by: 'inkonbtc',
   },
   {
+    name: 'banner_002',
+    by: 'inkonbtc',
+  },
+  {
     name: 'runic_001',
     by: 'sennettlau_13',
+  },
+  {
+    name: 'inkonbtc_001',
+    by: 'RENYYJ',
+  },
+  {
+    name: 'inkonbtc_002',
+    by: 'RENYYJ',
+  },
+  {
+    name: 'inkonbtc_003',
+    by: 'RENYYJ',
+  },
+  {
+    name: 'inkonbtc_004',
+    by: 'RENYYJ',
+  },
+  {
+    name: 'inkonbtc_005',
+    by: 'sennettlau_13',
+  },
+  {
+    name: 'inkonbtc_006',
+    by: 'RENYYJ',
+  },
+  {
+    name: 'inkonbtc_007',
+    by: 'sennettlau_13',
+  },
+  {
+    name: 'inkonbtc_008',
+    by: 'sennettlau_13',
+  },
+  {
+    name: 'hoponopono_001',
+    by: 'hoponopono88',
   },
   {
     name: 'hoponopono_002',
@@ -14,13 +54,5 @@ export const bannerConfig = [
   {
     name: 'rift_001',
     by: 'entertherifts',
-  },
-  {
-    name: 'banner_002',
-    by: 'inkonbtc',
-  },
-  {
-    name: 'hoponopono_001',
-    by: 'hoponopono88',
   },
 ]
