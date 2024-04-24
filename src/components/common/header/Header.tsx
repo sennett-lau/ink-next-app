289799594eee20b6b4748a56b6c3fb1f67a62cd8
Sennett Lau <laub1199@gmail.com>
2024-04-24 13:19:29 +0800

    rwd header update

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -3,9 +3,9 @@ import Image from 'next/image'
 const Header = () => {
   return (
     <header className='bg-header-bg fixed top-0 w-full bg-no-repeat bg-cover bg-center shadow-md'>
-      <div className='flex justify-between items-center w-full max-w-[1440px] mx-auto px-6'>
+      <div className='flex justify-center lg:justify-between items-center w-full max-w-[1440px] mx-auto px-6'>
         <Image src='/logo.png' alt='logo' width={80} height={70} />
-        <p className='font-black text-black text-2xl italic'>Ink Is For The People.</p>
+        <p className='font-black text-black text-2xl italic hidden lg:block'>Ink Is For The People.</p>
       </div>
     </header>
   )
