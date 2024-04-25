41eca8d1921beb110cad5f852f3a2e2d25518644
Sennett Lau <laub1199@gmail.com>
2024-04-25 13:20:44 +0800

    code refactor on CustomizerContainer

--- src/components/customizer/XBannerCustomizer.tsx
+++ src/components/customizer/XBannerCustomizer.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import { useEffect, useState } from 'react'
 import { bannerConfig } from '../../config/banners'
+import CustomizerContainer from './common/CustomizerContainer'
 import InkIdInput from './common/InkIdInput'
 
 type Props = {
@@ -31,60 +32,34 @@ const XBannerCustomizer = (props: Props) => {
   }
 
   return (
-    <>
-      <div className='flex flex-col w-full rounded-2xl bg-white bg-opacity-50 px-4 py-4 shadow-xl min-h-[400px]'>
-        <InkIdInput inkId={inkId} setInkId={setInkId} />
-        <p className='text-black italic font-bold text-lg mt-4 w-full text-left'>Choose a banner Background:</p>
-        <div className='flex flex-col gap-2 max-w-full'>
-          <div className='flex flex-col gap-2 w-full lg:min-h-[376px]'>
-            {banners.slice((bannerPage - 1) * 3, bannerPage * 3).map((banner, index) => (
-              <div className='max-w-full w-[365px] h-[120px] relative transform hover:scale-105 transition-transform duration-300'>
-                <a
-                  className='italic text-black text-md bg-white bg-opacity-75 rounded-br-lg p-2 absolute top-0 left-0'
-                  href={`https://x.com/${banner.by}`}
-                  target='_blank'
-                >
-                  @{banner.by}
-                </a>
-                <Image
-                  className={`cursor-pointer ${bannerBackground === banner.name ? 'max-w-full border-2 border-primary-500' : ''}`}
-                  key={index}
-                  src={`/assets/banner-backgrounds/${banner.name}.webp`}
-                  alt={banner.name}
-                  width={365}
-                  height={120}
-                  onClick={() => onBannerSelect(banner.name)}
-                />
-              </div>
-            ))}
-          </div>
-          <div className='flex'>
-            <p className='text-black italic font-bold w-full text-left'>
-              Page {bannerPage} of {bannerTotalPages}
-            </p>
-            <button
-              onClick={() => setBannerPage((prev) => (prev === 1 ? prev : prev - 1))}
-              className='mr-2 text-black font-bold'
-            >
-              {'<'}
-            </button>
-            <input
-              className='bg-transparent border-b-2 border-black text-black font-bold pl-2 focus:outline-none w-[24px]'
-              type='text'
-              value={bannerPage}
-              onChange={(e) => setBannerPage(parseInt(e.target.value))}
-              disabled
-            />
-            <button
-              onClick={() => setBannerPage((prev) => (prev === bannerTotalPages ? prev : prev + 1))}
-              className='ml-2 text-black font-bold'
-            >
-              {'>'}
-            </button>
-          </div>
+    <CustomizerContainer pageIndex={bannerPage} setPageIndex={setBannerPage} totalPages={bannerTotalPages}>
+      <InkIdInput inkId={inkId} setInkId={setInkId} />
+      <p className='text-black italic font-bold text-lg mt-4 w-full text-left'>Choose a banner Background:</p>
+      <div className='flex flex-col gap-2 max-w-full'>
+        <div className='flex flex-col gap-2 w-full lg:min-h-[376px]'>
+          {banners.slice((bannerPage - 1) * 3, bannerPage * 3).map((banner, index) => (
+            <div className='max-w-full w-[365px] h-[120px] relative transform hover:scale-105 transition-transform duration-300'>
+              <a
+                className='italic text-black text-md bg-white bg-opacity-75 rounded-br-lg p-2 absolute top-0 left-0'
+                href={`https://x.com/${banner.by}`}
+                target='_blank'
+              >
+                @{banner.by}
+              </a>
+              <Image
+                className={`cursor-pointer ${bannerBackground === banner.name ? 'max-w-full border-2 border-primary-500' : ''}`}
+                key={index}
+                src={`/assets/banner-backgrounds/${banner.name}.webp`}
+                alt={banner.name}
+                width={365}
+                height={120}
+                onClick={() => onBannerSelect(banner.name)}
+              />
+            </div>
+          ))}
         </div>
       </div>
-    </>
+    </CustomizerContainer>
   )
 }
 
